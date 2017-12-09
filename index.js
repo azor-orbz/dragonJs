c713fc0e6d864d80ba653ce62224c5bb2aab6e8f
AZOR <azor.haf@gmail.com>
2017-12-09 22:17:55 +0100

    Still dummy starting

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var readline = require('readline');
+
 var questions = new Map();
 
 let actual_stage = 0;
 let _MAIN_MENU = -1;
 let _STAGE_QUESTION_NAME = 1;
 let _STAGE_QUESTION_TEXT = 2;
+let _STAGE_QUESTION_TOPICS = 3;
+let _STAGE_QUESTION_DIFFICULTY = 4;
+let _STAGE_QUESTION_STAGE = 5;
 let _MAIN_MENU_QUESTION = "What do you want to do: ";
 
 
@@ -40,7 +44,45 @@ let menuItems = [
         action: exit
     },
 ];
+let validTopics = ['javascript-core', 'node-js', 'node-certification', '70-480'];
 
+function printValidTopics() {
+    let select = "Please select one of the following topics: ";
+    let command;
+    let pos = 0;
+    console.log(select);
+    for (let top of validTopics) {
+        pos++;
+        command = `${pos} : ${top}`;
+        console.log(command);
+    }
+}
+
+let topicMenuContinue = [{
+    idAction: 1,
+    text: 'Add another topic(s)',
+    actionCommand: "1",
+    action: function (a) {
+        defineQuestionStage3(a);
+    }
+}, {
+    idAction: 2,
+    text: 'Continue',
+    actionCommand: "2",
+    action: function (a) {
+        defineQuestionStage5(a);
+    }
+}
+];
+
+function printMenuTopicContinue() {
+    let line = "";
+    for (i of topicMenuContinue) {
+        line = ` ${i.idAction} (command: ${i.actionCommand}) :  ${i.text}  `;
+        console.log(line);
+    }
+
+}
 
 function printMenu() {
     let line = "";
@@ -52,6 +94,7 @@ function printMenu() {
 
 
 function exit() {
+    console.log("Game end");
     process.exit(1);
 }
 
@@ -64,9 +107,13 @@ console.log("The game has started");
 printMenu();
 ask(_MAIN_MENU_QUESTION);
 
-function ask(lquestion) {
+function ask(lquestion, call) {
     rl.question(lquestion, function (a) {
-            evaluateDecision(a);
+            if (call !== undefined) {
+                call(a);
+            } else {
+                evaluateDecision(a);
+            }
         }
     );
 }
@@ -93,13 +140,22 @@ function evaluateDecision(answer) {
 rl.on('line', function (a) {
     switch (actual_stage) {
         case _STAGE_QUESTION_NAME:
-            defineQuestionStage1(a);
+            defineQuestionStage2(a);
             break;
         case _STAGE_QUESTION_TEXT:
             defineQuestionStage2(a);
             break;
         case _MAIN_MENU:
             evaluateDecision(a);
+            break;
+        case _STAGE_QUESTION_TOPICS:
+            defineQuestionStage4(a);
+            break;
+        case _STAGE_QUESTION_DIFFICULTY:
+            defineQuestionStage5(a);
+            break;
+        case _STAGE_QUESTION_STAGE:
+            defineQuestionStage6(a);
             break;
         default:
             console.log("Missing action for:" + actual_stage);
@@ -108,12 +164,41 @@ rl.on('line', function (a) {
 });
 
 function defineQuestionStage1() {
-    let q = "Please Enter the question:";
+    let q = "Please Enter the question: ";
     actual_stage = _STAGE_QUESTION_NAME;
-    ask(q);
+    ask(q, defineQuestionStage2);
 }
 
 function defineQuestionStage2(a) {
-    let q = "Please Enter the questions text";
-    ask(q);
+    let q = "Please Enter the questions text: ";
+    actual_stage = _STAGE_QUESTION_TEXT;
+    ask(q, defineQuestionStage3);
+}
+
+function defineQuestionStage3(a) {
+    printValidTopics();
+    actual_stage = _STAGE_QUESTION_TOPICS;
+    ask("Topics:", defineQuestionStage4);
+}
+
+function defineQuestionStage4(a) {
+    if (a <= validTopics.length) {
+        console.log("Topics selected: " + validTopics[a - 1]);
+        actual_stage = _STAGE_QUESTION_DIFFICULTY;
+        printMenuTopicContinue();
+    } else {
+        console.log("Not valid topic");
+        actual_stage = _STAGE_QUESTION_TOPICS;
+        ask("Topics:", defineQuestionStage4);
+    }
+
+}
+function defineQuestionStage5(a) {
+    let q = "What is the difficulties of the question? (1-9)";
+    actual_stage = _STAGE_QUESTION_STAGE;
+    ask("Topics:", defineQuestionStage6);
+}
+function defineQuestionStage6(a) {
+    new Error("TODO");
+
 }
